c5ae1bc3b7bfe00f966bfc9931d39c3e96a80b69
Michael Wittig <post@michaelwittig.info>
2017-10-27 23:44:43 +0200

    first draft

---
+++ robot-controller/controller.js
@@ -0,0 +1,82 @@
+const request = require('request-promise-native');
+
+const ENDPOINT_URL = 'http://10.200.21.55:8080';
+
+const sendMove = (x, y, z) => 
+  request({
+    method: 'POST',
+    uri: `${ENDPOINT_URL}/v1/opcua`,
+    json: true,
+    body: {
+      "ascOpcuaProfiles": [{
+        "id":"1",
+        "lrXPos": `${x}`,
+        "lrYPos": `${y}`,
+        "lrZPos": `${z}`,
+        "lrAux1Pos": "250",
+        "lrPathVel":"1000",
+        "lrPathAcc":"10000",
+        "lrPathDec":"10000",
+        "lrBlendingRadius":"100"
+      }, {
+        "id":"2",
+        "lrXPos": `${x}`,
+        "lrYPos": `${y}`,
+        "lrZPos": `${z}`,
+        "lrAux1Pos": "250",
+        "lrPathVel":"0",
+        "lrPathAcc":"10000",
+        "lrPathDec":"10000",
+        "lrBlendingRadius":"100"
+      }],
+      "w_Mode_Select_Opc":"7",
+      "x_StartMode":true,
+      "x_AbortMode":false,
+      "x_ResetErrorOpc":false,
+      "eSelectAxisOpc":"Z"
+    }
+  });
+
+const stopMove = () => 
+  request({
+    method: 'POST',
+    uri: `${ENDPOINT_URL}/v1/opcua`,
+    json: true,
+    body: {
+      "ascOpcuaProfiles": [],
+      "w_Mode_Select_Opc":"7",
+      "x_StartMode":false,
+      "x_AbortMode":false,
+      "x_ResetErrorOpc":false,
+      "eSelectAxisOpc":"Z"
+    }
+  });
+
+const awaitMove = () => 
+  request({
+    method: 'GET',
+    uri: `${ENDPOINT_URL}/v1/opcua/module/state`,
+    json: true
+  }).
+    then((body) => {
+      console.log(body.xDone);
+      if (body.xDone === true) {
+        return body;
+      } else {
+        return awaitMove();
+      }
+    });
+
+const move = (x, y, z) => 
+  sendMove(x, y, z)
+    .then(stopMove)
+    .then(awaitMove);
+
+const grip = close => {};
+
+
+//x -250 - 250
+//y -210 - 210
+//z -625 - -685
+move(-100, 50, -685)
+  .then(() => console.log('done'));
