b684dfbff1d2520737b6db4d4e01fd1494ac7654
Thorsten Hoeger <thorsten.hoeger@taimos.de>
2017-10-27 22:10:46 +0200

    initial structure

---
+++ data-fetcher/index.js
@@ -0,0 +1,22 @@
+const request = require('request');
+
+const getLoginToken = () => {
+  'use strict';
+  
+};
+
+const fetchVehicles = auth => {
+  'use strict';
+  
+};
+
+const parsePositions = vehicles => {
+  'use strict';
+  
+};
+
+getLoginToken()
+  .then(fetchVehicles)
+  .then(parsePositions)
+  .then(console.log);
+
