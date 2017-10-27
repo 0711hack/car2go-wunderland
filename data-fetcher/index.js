f04e752080f535cb7ad8919ed13edead4f80595e
Thorsten Hoeger <thorsten.hoeger@taimos.de>
2017-10-27 23:15:19 +0200

    extract car2go service

--- data-fetcher/index.js
+++ data-fetcher/index.js
@@ -1,61 +1,7 @@
-const request = require('request-promise-native');
-const config = require('./config.json');
+const car2go = require('./lib/car2go');
 
-const getLoginToken = () => {
-  'use strict';
-  
-  let data = {
-    serviceRequest: {
-      payLoadData: {
-        emailAddress: config.loginEmail,
-        password: config.loginPassword
-      }
-    }
-  };
-  
-  return request.post({
-    url: 'https://common-api-hackathon2017.car2go.io/public/login',
-    body: data,
-    json: true
-  }).then(res => {
-    let authorization = res.serviceResponse.payLoadData.authorization;
-    if (!authorization) {
-      console.log(JSON.stringify(res, null, 2));
-      throw 'LoginError';
-    }
-    return {
-      user: authorization.userId,
-      token: authorization.accessToken
-    };
-  }).catch(err => {
-    console.log(err);
-    throw 'LoginError';
-  });
-};
-
-const fetchVehicles = auth => {
-  'use strict';
-  return request.get({
-    url: 'https://fmm-api-hackathon2017.car2go.io/protected/vehicles',
-    headers: {
-      userId: auth.user,
-      accessToken: auth.token
-    },
-    json: true
-  }).then(vehicles => {
-    console.log(JSON.stringify(vehicles, null, 2));
-    return vehicles;
-  });
-};
-
-const parsePositions = vehicles => {
-  'use strict';
-  
-};
-
-getLoginToken()
-  .then(fetchVehicles)
-  .then(parsePositions)
+car2go.login()
+  .then(car2go.fetchVehicles)
   .then(console.log)
   .catch(err => {
     'use strict';
---
+++ data-fetcher/lib/car2go.js
@@ -0,0 +1,60 @@
+const request = require('request-promise-native');
+const config = require('../config.json');
+
+const parseLogin = res => {
+  'use strict';
+  let authorization = res.serviceResponse.payLoadData.authorization;
+  if (!authorization) {
+    console.log(JSON.stringify(res, null, 2));
+    throw 'LoginError';
+  }
+  return {
+    user: authorization.userId,
+    token: authorization.accessToken
+  };
+};
+
+const parseVehicles = res => {
+  'use strict';
+  return res.serviceResponse.payLoadData.vehicles.map(vehicle => {
+    return {
+      id: vehicle['_id'],
+      lat: vehicle.lat,
+      lon: vehicle.lon
+    };
+  });
+};
+
+exports.login = () => {
+  'use strict';
+  
+  let authData = {
+    serviceRequest: {
+      payLoadData: {
+        emailAddress: config.loginEmail,
+        password: config.loginPassword
+      }
+    }
+  };
+  
+  return request.post({
+    url: 'https://common-api-hackathon2017.car2go.io/public/login',
+    body: authData,
+    json: true
+  }).then(parseLogin).catch(err => {
+    console.log(err);
+    throw 'LoginError';
+  });
+};
+
+exports.fetchVehicles = authData => {
+  'use strict';
+  return request.get({
+    url: 'https://fmm-api-hackathon2017.car2go.io/protected/vehicles',
+    headers: {
+      userId: authData.user,
+      accessToken: authData.token
+    },
+    json: true
+  }).then(parseVehicles);
+};
