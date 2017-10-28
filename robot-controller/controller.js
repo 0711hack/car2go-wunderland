1fa11bc817b5954eb3baa7f5b4720d87538ccf4c
Michael Wittig <post@michaelwittig.info>
2017-10-28 10:11:51 +0200

    implemented new move api

--- robot-controller/controller.js
+++ robot-controller/controller.js
@@ -1,8 +1,15 @@
 const request = require('request-promise-native');
 
 const ENDPOINT_URL = 'http://10.200.21.55:8080';
+const Z_DOWN = '-685';
+const Z_UP = '-670';
+const VELOCITY = '1000';
+const ACC = '10000';
+const DEC = '10000';
+const AUX1POS = '250';
+const Blending_READIUS = '100';
 
-const sendMove = (x, y, z) => 
+const sendMove = (x1, y1, x2, y2) => 
   request({
     method: 'POST',
     uri: `${ENDPOINT_URL}/v1/opcua`,
@@ -10,24 +17,54 @@ const sendMove = (x, y, z) =>
     body: {
       ascOpcuaProfiles: [{
         id: '1',
-        lrXPos: `${x}`,
-        lrYPos: `${y}`,
-        lrZPos: `${z}`,
-        lrAux1Pos: '250',
-        lrPathVel: '1000',
-        lrPathAcc: '10000',
-        lrPathDec: '10000',
-        lrBlendingRadius: '100'
+        lrXPos: `${x1}`,
+        lrYPos: `${y1}`,
+        lrZPos: Z_DOWN,
+        lrAux1Pos: AUX1POS,
+        lrPathVel: VELOCITY,
+        lrPathAcc: ACC,
+        lrPathDec: DEC,
+        lrBlendingRadius: Blending_READIUS
       }, {
         id: '2',
-        lrXPos: `${x}`,
-        lrYPos: `${y}`,
-        lrZPos: `${z}`,
-        lrAux1Pos: '250',
+        lrXPos: `${x1}`,
+        lrYPos: `${y1}`,
+        lrZPos: Z_UP,
+        lrAux1Pos: AUX1POS,
+        lrPathVel: VELOCITY,
+        lrPathAcc: ACC,
+        lrPathDec: DEC,
+        lrBlendingRadius: Blending_READIUS
+      }, {
+        id: '3',
+        lrXPos: `${x2}`,
+        lrYPos: `${y2}`,
+        lrZPos: Z_UP,
+        lrAux1Pos: AUX1POS,
+        lrPathVel: VELOCITY,
+        lrPathAcc: ACC,
+        lrPathDec: DEC,
+        lrBlendingRadius: Blending_READIUS
+      }, {
+        id: '4',
+        lrXPos: `${x2}`,
+        lrYPos: `${y2}`,
+        lrZPos: Z_DOWN,
+        lrAux1Pos: AUX1POS,
+        lrPathVel: VELOCITY,
+        lrPathAcc: ACC,
+        lrPathDec: DEC,
+        lrBlendingRadius: Blending_READIUS
+      }, {
+        id: '5',
+        lrXPos: `${x2}`,
+        lrYPos: `${y2}`,
+        lrZPos: Z_DOWN,
+        lrAux1Pos: AUX1POS,
         lrPathVel: '0', // set velocity to zero to stop the move
-        lrPathAcc: '10000',
-        lrPathDec: '10000',
-        lrBlendingRadius: '100'
+        lrPathAcc: ACC,
+        lrPathDec: DEC,
+        lrBlendingRadius: Blending_READIUS
       }],
       w_Mode_Select_Opc: '7',
       x_StartMode: true,
@@ -68,7 +105,7 @@ const awaitMove = () =>
     });
 
 const move = (x1, y1, x2, y2) => 
-  sendMove(x, y, z)
+  sendMove(x1, y1, x2, y2)
     .then(stopMove)
     .then(awaitMove);
 
@@ -79,6 +116,6 @@ exports.move = (x1, y1, x2, y2) => move(x1, y1, x2, y2);
 
 //x -250 - 250
 //y -210 - 210
-//z -625 - -685
-move(-100, 50, -685)
+//z -685 - -625
+move(-100, 50, -200, 100)
   .then(() => console.log('done'));
