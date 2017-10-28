22a59194446f3d14bb404b05d96142e0174641db
Michael Wittig <post@michaelwittig.info>
2017-10-28 15:33:46 +0200

    connext robot

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 const data = require('./data-fetcher');
-const wonderland = require('./wonderland');
+const wonderland = require('./wonderland/lib/wonderland.js');
+const robot = require('./robot-controller/controller.js');
+
+const processMoves = ([head, ...tail]) => 
+  robot.move(head.from.x, head.from.y, head.to.x, head.to.y)
+    .then(() => {
+      if (tail.length === 0) {
+        return true;
+      } else {
+        return processMoves(tail)
+      }
+    });
 
 const process = () => {
   'use strict';
@@ -10,6 +21,7 @@ const process = () => {
       return list;
     })
     .then(wonderland.setVehicles)
+    .then(() => processMoves(wonderland.getMoves()))
     .catch(err => {
       console.log(`Program failed due to: ${err}`);
       return new Promise(resolve => {
