3b0e0a6b79febdb4e9f7d5775fdb6453366e13d1
Thorsten Hoeger <thorsten.hoeger@taimos.de>
2017-10-29 09:47:57 +0100

    prevent duplicates in second run

--- wonderland/lib/wonderland.js
+++ wonderland/lib/wonderland.js
@@ -4,12 +4,12 @@ const park_offset = 232.957960406; // 5 cm
 var currentState = []
 var targetState = []
 
-function array_diff(left, rigth) {
+function array_diff(left, right) {
   var result = [];
   for (var i = 0; i < left.length; i++) {
     var miss = true;
-    for (var j = 0; j < rigth.length; j++) {
-      if (left[i].x == rigth[j].x && left[i].y == rigth[j].y) {
+    for (var j = 0; j < right.length; j++) {
+      if (left[i].x === right[j].x && left[i].y === right[j].y) {
         miss = false;
       }
     }
@@ -35,39 +35,69 @@ function parkVehicles(vehicles) {
   return filledVehicles;
 }
 
-exports.load = function() {
+exports.load = function () {
   'use strict';
   currentState = JSON.parse(fs.readFileSync(`${__dirname}/../state.json`));
   return Promise.resolve(true);
 };
-exports.save = function() {
+exports.save = function () {
   'use strict';
   fs.writeFileSync(`${__dirname}/../state.json`, JSON.stringify(currentState));
   return Promise.resolve(true);
 };
-exports.setVehicles = function(vehicles) {
+exports.setVehicles = function (vehicles) {
   'use strict';
-  if(vehicles.length > maxVehicleCount){
-    vehicles = vehicles.slice(0,maxVehicleCount);
+  if (vehicles.length > maxVehicleCount) {
+    vehicles = vehicles.slice(0, maxVehicleCount);
   }
   targetState = parkVehicles(vehicles);
-
+  
 };
-exports.getMoves = function() {
+exports.getMoves = function () {
   'use strict';
   var moves = [];
-
+  
   var changesCurrent = array_diff(targetState, currentState);
   var changesTarget = array_diff(currentState, targetState);
-  if (changesCurrent.length != changesTarget.length) {
-    console.log("Error, no match for length");
+  if (changesCurrent.length !== changesTarget.length) {
+    console.log('Error, no match for length');
   }
   for (var i = 0; i < changesCurrent.length; i++) {
     moves.push({
-      "from":currentState[changesCurrent[i]],
-      "to":targetState[changesTarget[i]]
+      from: currentState[changesCurrent[i]],
+      to: targetState[changesTarget[i]]
     });
   }
+  let newMoves = [];
+  moves.each((move, idx) => {
+    let toRemove = false;
+    for (let j = 0; j < currentState.length; j++) {
+      if (isNearby(move.to.x, move.to.y, currentState[j].x, currentState[j].y)) {
+        toRemove = true;
+      }
+    }
+    for (let i = idx + 1; i < moves.length; i++) {
+      if (isNearby(move.to.x, move.to.y, moves[i].to.x, moves[i].to.y)) {
+        toRemove = true;
+      }
+    }
+  
+    if (toRemove) {
+      for (let k = 0; k < targetState.length; k++) {
+        if (targetState[k].x === move.to.x && targetState[k].y === move.to.y) {
+          targetState[k].x = move.from.x;
+          targetState[k].y = move.from.y;
+        }
+      }
+    } else {
+      newMoves.push(move);
+    }
+  });
   currentState = targetState;
-  return moves;
+  return newMoves;
+};
+
+const isNearby = (x1, y1, x2, y2) => {
+  'use strict';
+  return Math.abs(x1 - x2) < 232 && Math.abs(y1 - y2) < 232;
 };
